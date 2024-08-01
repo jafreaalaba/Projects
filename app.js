0c5822aae66d8e19e00bf43263376da777995486
jafreaalaba <jafreaalaba@gmail.com>
2024-08-01 10:47:50 -0600

    My First Commit

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express ();
 const PORT = 3000;
 
-app.get('/', (req, res) =>{
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    res.send(`User with ID ${userId}`);
+});
+app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 

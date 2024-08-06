9b752c8e8854403825f228b09ba2e6ff2a714537
jafreaalaba <jafreaalaba@gmail.com>
2024-08-06 09:21:49 -0600

    My Second Commit

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express ();
 const PORT = 3000;
 
+
+
+
+
+app.set('view engine', 'ejs');
+app.set('viers', './views');
+
+
+//route controller with a path parameter
 app.get('/users/:id', (req, res) => {
     const userId = req.params.id;
     res.send(`User with ID ${userId}`);
 });
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    const data = {
+        title: 'Hello World!',
+        message: 'Welcome to my website!'
+    }
 });
 
 app.listen(3000, () => {
